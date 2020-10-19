dc5d701d469efe84bfa8a48f895db817c7c8752b
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-10-19 09:22:55 -0400

    chore: formatting

--- server/aws/elasticache.tf
+++ server/aws/elasticache.tf
@@ -1,11 +1,11 @@
 resource "aws_elasticache_subnet_group" "covidshield" {
-  count = (var.feature_redis ? 1 : 0)
+  count      = var.feature_redis ? 1 : 0
   name       = "covidshield"
   subnet_ids = aws_subnet.covidshield_private.*.id
 }
 
 resource "aws_elasticache_replication_group" "covidshield" {
-  count = (var.feature_redis ? 1 : 0)
+  count                         = var.feature_redis ? 1 : 0
   automatic_failover_enabled    = true
   availability_zones            = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1], data.aws_availability_zones.available.names[2]]
   replication_group_id          = "covidshield"
