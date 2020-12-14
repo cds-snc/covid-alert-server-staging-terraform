c1d7e102322d9a4a789a76c897a44d029bb9a8fa
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-12-14 12:34:07 -0500

    feat: enabling versioning for the state (#63)
    * feat: enabling versioning for the state
    
    *Please Note:* This has all been implemented already locally, this is
    just syncing it
    
    Enables versioning.
    Removes the unused log bucket since CBS is already logging
    Removed extra bootstrap files that were not used.
    
    * chore: remove CBS logging

--- bootstrap/provider.tf
+++ bootstrap/provider.tf
@@ -1,27 +1,24 @@
 provider "aws" {
   version = "~> 2.0"
   region  = "ca-central-1"
+  allowed_account_ids = [ "005133826942" ]
 }
 
 resource "aws_s3_bucket" "storage_bucket" {
   bucket = var.storage_bucket
+
   acl    = "private"
-  logging {
-    target_bucket = "${aws_s3_bucket.log_bucket.id}"
-    target_prefix = "log/"
+  versioning {
+    enabled = true
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = var.storage_bucket
-  acl    = "private"
+
+  # CBS is already logging managed outside of TF
   #tfsec:ignore:AWS002
+
+  # logging {
+  #   target_bucket = aws_s3_bucket.log_bucket.id
+  #   target_prefix = "log/"
+  # }
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -45,7 +42,6 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
     ("CostCentre") = "CovidShield"
   }
 }
-
 
 resource "aws_route53_zone" "covidshield" {
   name = var.route53_zone_name
