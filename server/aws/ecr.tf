3eb857da78dfaf1a031a3ab081ee1da61e0592e0
Calvin Rodo <calvin.rodo@cds-snc.ca>
2021-01-15 15:43:40 -0500

    feat: make ECR image tags immutable

--- server/aws/ecr.tf
+++ server/aws/ecr.tf
@@ -5,7 +5,7 @@ locals {
 resource "aws_ecr_repository" "repository" {
   for_each             = toset(local.image_names)
   name                 = "covid-server/${each.value}"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
