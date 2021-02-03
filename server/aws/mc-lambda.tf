132302e488083e9063504609909fe88afd773bf7
Tim jodoin <tim.jodoin@gmail.com>
2021-02-03 02:21:49 -0500

    fix tightened egress on lambda

--- server/aws/mc-lambda.tf
+++ server/aws/mc-lambda.tf
@@ -40,8 +40,8 @@ resource "aws_security_group" "lambda_sg" {
   vpc_id      = aws_vpc.covidshield.id
 
   egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 443
+    to_port     = 443
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
