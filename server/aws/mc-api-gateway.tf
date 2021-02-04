a1ac2aca695b8f122b5c0017c0f6ddac14ef585e
Calvin Rodo <calvin.rodo@cds-snc.ca>
2021-02-04 14:29:42 -0500

    fix: attach validator to POST (#96)

--- server/aws/mc-api-gateway.tf
+++ server/aws/mc-api-gateway.tf
@@ -54,11 +54,12 @@ resource "aws_api_gateway_resource" "create_resource" {
 }
 
 resource "aws_api_gateway_method" "create_method" {
-  rest_api_id      = aws_api_gateway_rest_api.metrics.id
-  resource_id      = aws_api_gateway_resource.create_resource.id
-  http_method      = "POST"
-  authorization    = "NONE"
-  api_key_required = true
+  rest_api_id          = aws_api_gateway_rest_api.metrics.id
+  resource_id          = aws_api_gateway_resource.create_resource.id
+  http_method          = "POST"
+  authorization        = "NONE"
+  api_key_required     = true
+  request_validator_id = aws_api_gateway_request_validator.metrics_model.id
 }
 
 resource "aws_api_gateway_method_response" "response_200" {
