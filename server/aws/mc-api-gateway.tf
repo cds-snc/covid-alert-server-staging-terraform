b1b3ff301697402b79a2359dcd9147d27f4f43ac
Calvin Rodo <calvin.rodo@cds-snc.ca>
2021-02-05 16:04:55 -0500

    fix: use triggers instead of variables

--- server/aws/mc-api-gateway.tf
+++ server/aws/mc-api-gateway.tf
@@ -114,9 +114,10 @@ resource "aws_api_gateway_integration_response" "metrics_response" {
 resource "aws_api_gateway_deployment" "metrics" {
   rest_api_id = aws_api_gateway_rest_api.metrics.id
 
-  # force a deploy
-  variables {
-    deployed_at = timestamp()
+  # Forces a redeploy of the gateway
+  # TODO: use a hash of create_metric.js, mc-*.tf files to trigger redeployment
+  triggers {
+    redeployment = timestamp()
   }
 }
 
