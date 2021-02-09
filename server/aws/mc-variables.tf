678b4b27a5d35777caae462169fe8bc2b1fc99c2
Calvin Rodo <calvin.rodo@cds-snc.ca>
2021-02-09 09:36:07 -0500

    fix: remove unused and sync with prod (#111)

--- server/aws/mc-variables.tf
+++ server/aws/mc-variables.tf
@@ -8,34 +8,10 @@ variable "service_name" {
   default     = "save-metrics"
 }
 
-variable "owner" {
-  type        = string
-  description = "Name of the owner"
-  default     = "CDS"
-}
-
-variable "project" {
-  type        = string
-  description = "Name of the project"
-  default     = "COVID-Alert"
-}
-
-variable "lambda_code" {
-  type        = string
-  description = "Name of the project"
-  default     = "cds-terraform-lambda-zips"
-}
-
 variable "s3_raw_metrics_bucket_name" {
   type        = string
   description = "S3 bucket that holds the raw mentric counts"
-  default     = "cds-covid-alert-bucket-dev"
-}
-
-variable "s3_raw_metrics_bucket_logging_name" {
-  type        = string
-  description = "S3 bucket that holds the logs for the metrics bucket"
-  default     = "cds-covid-alert-bucket-logging-dev"
+  default     = "in-app-metrics"
 }
 
 variable "api-description" {
@@ -43,29 +19,14 @@ variable "api-description" {
   default = "Terraform Serverless api for COVID Alert metrics collection"
 }
 
-variable "mks-description" {
-  type    = string
-  default = "This CMK is used to encrypt the metrics bucket"
-}
-
 variable "lambda-description" {
   type    = string
   default = "Lambda function for collecting metrics records"
 }
 
-variable "lambda-function-code" {
-  type    = string
-  default = "createRecord.zip"
-}
-
 variable "lambda-function-runtime" {
   type    = string
   default = "nodejs12.x"
-}
-
-variable "lambda-function-handler" {
-  type    = string
-  default = "index.handler"
 }
 
 variable "waf-description" {
@@ -77,11 +38,6 @@ variable "apiKeyName" {
   type        = string
   description = "API Key Name"
   default     = "Dev-Key"
-}
-
-variable "apiKeyDescription" {
-  type    = string
-  default = "API Key for Development"
 }
 
 variable "api_gateway_rate" {
