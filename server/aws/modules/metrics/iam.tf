fdbe23d66ff0b1b0fe2e71943fe6f362855a7ef1
Calvin Rodo <calvin.rodo@cds-snc.ca>
2021-02-09 13:55:34 -0500

    fix: use role name instead of arn (#114)

--- server/aws/modules/metrics/iam.tf
+++ server/aws/modules/metrics/iam.tf
@@ -1,26 +1,26 @@
 
-data "aws_iam_policy_document" "raw_metrics_put" { 
-  statement { 
+data "aws_iam_policy_document" "raw_metrics_put" {
+  statement {
     effect = "Allow"
 
-    actions = [ 
+    actions = [
       "dynamodb:PutItem"
     ]
 
-    resources = [ 
+    resources = [
       aws_dynamodb_table.raw_metrics.arn
     ]
   }
 
 }
 
-resource "aws_iam_policy" "raw_metrics_put" { 
-  name = "CovidAlertRawMetricsPutItem"
-  path = "/"
+resource "aws_iam_policy" "raw_metrics_put" {
+  name   = "CovidAlertRawMetricsPutItem"
+  path   = "/"
   policy = data.aws_iam_policy_document.raw_metrics_put.json
 }
 
 resource "aws_iam_role_policy_attachment" "raw_metrics_put" {
-  role       = var.role_arn
+  role       = var.role_name
   policy_arn = aws_iam_policy.raw_metrics_put.arn
 }
