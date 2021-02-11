9c32121767f4525f8a26a6b7f55f3fa8c024ff7e
Max Neuvians <max@neuvians.net>
2021-02-11 10:59:51 -0500

    fix: localize resources in module

--- server/aws/modules/metrics/iam_policies.tf
+++ server/aws/modules/metrics/iam_policies.tf
@@ -149,7 +149,7 @@ data "aws_iam_policy_document" "write_and_encrypt_deadletter_queue" {
 
     ]
     resources = [
-      aws_kms_key.cw.arn,
+      aws_kms_key.metrics_key.arn,
       aws_sqs_queue.aggregation_lambda_dead_letter.arn
     ]
 
