c25751b91ee7649d5b0651c1cfefce1f60b50b87
Max Neuvians <max.neuvians@cds-snc.ca>
2021-03-08 15:39:26 -0500

    fix: avoid timezone issues - max bound

--- server/aws/lambda/aggregate_values.test.js
+++ server/aws/lambda/aggregate_values.test.js
@@ -233,7 +233,6 @@ describe("generatePayload", () => {
 describe("pinDate", () => {
   it("rounds a unix timestamp to the current UTC date", () => {
     expect(lambda.pinDate(1615231884409)).toStrictEqual("2021-03-08")
-    expect(lambda.pinDate(2147483647000)).toStrictEqual("2038-01-18")
   })
 })
 
