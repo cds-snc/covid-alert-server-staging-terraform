0096d53863391e8bd6fccbef55e333d00b7eacc0
Pat Heard <patrick.heard@cds-snc.ca>
2021-05-06 12:36:28 +0000

    fix: install pinned Terraform/Terragrunt versions
    Also adds tfsec and removes host Docker access since it was
    only being used to run tfsec scans with their Docker image.

--- .devcontainer/library-scripts/asdf-terraform.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-. ~/.zshrc
-
-cd server/aws
-
-asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
-asdf plugin-add terragrunt https://github.com/lotia/asdf-terragrunt.git
-asdf install
