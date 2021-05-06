467ee434a71d7ca969ebf4f76ac7bd538091f805
Calvin Rodo <calvin.rodo@cds-snc.ca>
2021-05-06 17:02:58 -0400

    feat: removed csv_etl (#220)

--- server/aws/modules/metrics/ecr.tf
+++
@@ -1,9 +0,0 @@
-
-resource "aws_ecr_repository" "create_csv" {
-  name                 = "covid-server/metrics-server"
-  image_tag_mutability = "MUTABLE"  # tfsec:ignore:AWS078  `latest` tags are used in Staging
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
