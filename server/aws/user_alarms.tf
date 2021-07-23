b3ffca8fcd3c912b037c43e9cd972833fed8aaaa
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-07-23 11:33:22 -0400

    fix: reference module properly

--- server/aws/user_alarms.tf
+++ server/aws/user_alarms.tf
@@ -1,5 +1,5 @@
 module "ops_alarms" {
-  source                = "github.com/cds-snc/terraform-modules//user_login_alarm@v0.01"
+  source                = "github.com/cds-snc/terraform-modules?ref=v0.0.1//user_login_alarm"
   account_names         = ["ops1", "ops2"]
   alarm_action_failures = [aws_sns_topic.alert_warning.arn]
   alarm_action_success  = [aws_sns_topic.alert_critical.arn]
