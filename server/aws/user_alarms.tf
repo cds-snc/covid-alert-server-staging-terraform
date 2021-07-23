ad2a1bdcc791bf4ecc5c9b865c588e30c0bb89c7
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-07-23 11:44:27 -0400

    feat: fix params

--- server/aws/user_alarms.tf
+++ server/aws/user_alarms.tf
@@ -1,8 +1,8 @@
 module "ops_alarms" {
   source                = "github.com/cds-snc/terraform-modules?ref=v0.0.1//user_login_alarm"
   account_names         = ["ops1", "ops2"]
-  alarm_action_failures = [aws_sns_topic.alert_warning.arn]
-  alarm_action_success  = [aws_sns_topic.alert_critical.arn]
+  alarm_actions_failure = [aws_sns_topic.alert_warning.arn]
+  alarm_actions_success = [aws_sns_topic.alert_critical.arn]
   log_group_name        = "CloudTrail/Landing-Zone-Logs"
   num_attempts          = 3
 }
