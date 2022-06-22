7605dd0b0fe165903293e3918afdc3feeaf54bbf
Pat Heard <patrick.heard@cds-snc.ca>
2022-06-22 14:56:56 +0000

    chore: initial delete of server resources
    Remove the server resources that are not required to serve
    the phone configuration files.

--- server/aws/outputs.tf
+++ server/aws/outputs.tf
@@ -6,10 +6,6 @@ output "cloudwatch_log_group" {
   value = aws_cloudwatch_log_group.covidshield
 }
 
-output "ecs_cluster" {
-  value = aws_ecs_cluster.covidshield
-}
-
 output "route53_zone" {
   value = aws_route53_zone.covidshield
 }
@@ -24,12 +20,4 @@ output "aws_public_subnets" {
 
 output "security_group_load_balancer" {
   value = aws_security_group.covidshield_load_balancer
-}
-
-output "aws_db_subnet_group" {
-  value = aws_db_subnet_group.covidshield
-}
-
-output "route53_submission_fqdn" {
-  value = aws_route53_record.covidshield_key_submission
 }
